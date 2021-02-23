fa1b594694afac13467320c91c7b29d5ec05a6b3
Simon Democko <simon.democko@magnolia-cms.com>
2021-02-23 21:36:42 +0100

    Add intersection observer wcomparing distance from top

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -18,22 +18,73 @@
  * 
 */
 
+function generateListOfSectionLinks() {
+    let sectionListItems = document.createDocumentFragment();
+    for (const element of getAllSections()) {
+        let entry = document.createElement("li");
+        sectionListItems.appendChild(entry);
+        let link = entry.appendChild(document.createElement("a"));
+        link.setAttribute("href", `#${element.getAttribute("id")}`);
+        link.textContent = element.getAttribute("data-nav");
+    }
+    return sectionListItems;
+}
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+function resetNavigation() {
+    let newNavigationList = generateListOfSectionLinks();
+    let navigation = getNavBarList();
+    navigation.innerHTML = "";
+    navigation.appendChild(newNavigationList);
+    console.log("dlkjf");
+}
 
+function handleStructureChange() {
+    resetNavigation();
+    for (const element of getAllSections()) {
+        observer.observe(element);
+    }
+}
 
+function getNavBarList() {
+    return document.getElementById("navbar__list");
+}
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+function getAllSections() {
+    return document.querySelectorAll("main > section");
+}
 
-// build the nav
+const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step)); // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+
+let options = {
+    threshold: range(0, 1, 0.10)
+}
+
+let observer = new IntersectionObserver(handleSectionViewportIntersect, options);
+
+document.addEventListener('DOMContentLoaded', handleStructureChange());
+new MutationObserver(() => handleStructureChange())
+    .observe(document.querySelector("main"), { childList: true });
+
+function handleSectionViewportIntersect(entries, observer) {
+    let closestElement;
+    let closestElementsDistanceFromTop = Number.MAX_VALUE;
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            let entrysDistanceFromTop = entry.boundingClientRect.y;
+            if (Math.abs(entrysDistanceFromTop) < closestElementsDistanceFromTop) {
+                closestElementsDistanceFromTop = entrysDistanceFromTop;
+                closestElement = entry.target;
+            }
+        }
+    });
+    for (const element of document.getElementsByClassName("your-active-class")){
+        if (closestElement && element != closestElement) {
+            element.classList.toggle("your-active-class");
+            closestElement.classList.toggle("your-active-class");
+        }   
+    }
+}
+
 
 
 // Add class 'active' to section when near top of viewport
@@ -45,7 +96,7 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
 // Build menu 
