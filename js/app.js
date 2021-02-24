c9929bf34281c283ebcb6abee18fe598acc1946f
Simon Democko <simon.democko@magnolia-cms.com>
2021-02-24 18:31:00 +0100

    Provide extensive redundant documentation to pass project rules
    One of the things I had to rewrite in the project was documentation of
    methods. I disagree with such approach, because I did everything to
    have functions
    - sufficiently small
    - self-explanatory,
    - named so that they communicate intent comprehensively.
    
    The added documentation is mostly a human-readable
    rewrite of the function names.
    
    Nevertheless, rules are rules, so I'm passing the documentation as
    required.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step)); // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
 let observer = new IntersectionObserver(handleIntersectionOfViewportWithSections, { threshold: range(0, 1, 0.10) });
 
+/**
+ * For dynamic building of navigation and related observations.
+ */
 function handleStructureChange() {
     resetNavigation();
     observer.disconnect()
@@ -13,6 +16,9 @@ function getAllSections() {
     return document.querySelectorAll("main > section");
 }
 
+/**
+ * Generates new navigation and puts it in place of the old one.
+ */
 function resetNavigation() {
     let newNavigationList = generateListOfSectionLinks();
     let navigation = getNavBarList();
@@ -20,10 +26,16 @@ function resetNavigation() {
     navigation.appendChild(newNavigationList);
 }
 
+/**
+ * Lookup element, where nevagiation takes place.
+ */
 function getNavBarList() {
     return document.getElementById("navbar__list");
 }
 
+/**
+ * Generates new navigation based on existing sections.
+ */
 function generateListOfSectionLinks() {
     let sectionListItems = document.createDocumentFragment();
     for (const element of getAllSections()) {
@@ -32,15 +44,24 @@ function generateListOfSectionLinks() {
     return sectionListItems;
 }
 
-function createNavigationElemForSection(element) {
+/**
+ * 
+ * @param {*} sectionElement 
+ * @return a styled navigation element for the sectionElement
+ */
+function createNavigationElemForSection(sectionElement) {
     let entry = document.createElement("li");
     let link = entry.appendChild(document.createElement("a"));
-    link.setAttribute("href", `#${element.getAttribute("id")}`);
-    link.textContent = element.getAttribute("data-nav");
+    link.setAttribute("href", `#${sectionElement.getAttribute("id")}`);
+    link.textContent = sectionElement.getAttribute("data-nav");
     link.classList.add("menu__link");
     return entry;
 }
 
+/**
+ * Handles observed intersection changes, especially for making styling changes.
+ * @param {IntersectionObserverEntry[]} entries 
+ */
 function handleIntersectionOfViewportWithSections(entries) {
     let closestEntry = findClosestIntersectingEntryToViewPortTop(entries);
     if (closestEntry && isObservedIntersectionCloseEnoughToViewportTop(closestEntry)) {
@@ -60,15 +81,23 @@ function handleIntersectionOfViewportWithSections(entries) {
  * @param {IntersectionObserverEntry} entry
  */
 function isObservedIntersectionCloseEnoughToViewportTop(entry) {
-    return getAbsoluteDistanceFromTopOfViewport(entry) < window.innerHeight * 0.4;
+    return Math.abs(entry.boundingClientRect.y) < window.innerHeight * 0.4;
 }
 
+/**
+ * For a specified newActiveSection makes appropriate styling changes.
+ * @param {*} newActiveSection 
+ */
 function resetActiveSectionStates(newActiveSection) {
     resetSectionsStyles(newActiveSection);
     resetNavigationStyling(newActiveSection);
 }
 
-function resetSectionsStyles(newActiveSection){
+/**
+ * Adds styling to activated section, removing it from non-active ones.
+ * @param {*} newActiveSection 
+ */
+function resetSectionsStyles(newActiveSection) {
     for (const element of document.getElementsByClassName("your-active-class")) {
         if (element != newActiveSection) {
             element.classList.remove("your-active-class");
@@ -77,13 +106,17 @@ function resetSectionsStyles(newActiveSection){
     newActiveSection.classList.add("your-active-class");
 }
 
+/**
+ * Adds styling to navigation reference of activated section, removing it from non-active ones.
+ * @param {*} newActiveSection 
+ */
 function resetNavigationStyling(newActiveSection) {
     let sectionId = newActiveSection.getAttribute("id");
     inactivateActiveNavigationLinkIfNotCurrent(sectionId);
     styleNavigationLinkOfActive(sectionId);
 }
 
-function inactivateActiveNavigationLinkIfNotCurrent(sectionId){
+function inactivateActiveNavigationLinkIfNotCurrent(sectionId) {
     for (const element of document.getElementsByClassName("menu__link_active")) {
         if (!element.getAttribute("href").endsWith("#" + sectionId)) {
             element.classList.remove("menu__link_active");
@@ -92,7 +125,7 @@ function inactivateActiveNavigationLinkIfNotCurrent(sectionId){
 }
 
 function styleNavigationLinkOfActive(sectionId) {
-    for (const link of document.getElementsByClassName("menu__link")){
+    for (const link of document.getElementsByClassName("menu__link")) {
         if (link.getAttribute("href").endsWith("#" + sectionId)) {
             link.classList.add("menu__link_active");
         }
@@ -109,17 +142,13 @@ function findClosestIntersectingEntryToViewPortTop(entries) {
     let closestEntry;
     let closestElementsDistanceFromTop = Number.MAX_VALUE;
     entries.forEach((entry) => {
-        let entrysDistanceFromTop = getAbsoluteDistanceFromTopOfViewport(entry);
+        let entrysDistanceFromTop = Math.abs(entry.boundingClientRect.y);
         if (entry.isIntersecting && entrysDistanceFromTop < closestElementsDistanceFromTop) {
             closestElementsDistanceFromTop = entrysDistanceFromTop;
             closestEntry = entry;
         }
     });
     return closestEntry;
-}
-
-function getAbsoluteDistanceFromTopOfViewport(entry) {
-    return Math.abs(entry.boundingClientRect.y)
 }
 
 document.addEventListener('DOMContentLoaded', handleStructureChange());
