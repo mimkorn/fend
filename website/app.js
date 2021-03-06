8c6139ba4928aecaa916572fd416806379bfe0ce
Simon Democko <simon.democko@magnolia-cms.com>
2021-03-06 16:00:54 +0100

    Add get data from server to client side

--- website/app.js
+++ website/app.js
@@ -3,3 +3,16 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+async function get() {
+
+    try {
+      let response = await fetch('/all');
+      let data = await response.json();
+    } catch(err) {
+      // catches errors both in fetch and response.json
+      alert(err);
+    }
+  }
+  
+get();
