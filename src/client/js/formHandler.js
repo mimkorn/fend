ee99062685f088492687d59faaaaf428cf19ff47
Simon Democko <simon.democko@magnolia-cms.com>
2021-03-23 17:58:10 +0100

    Add URL validation

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,11 +1,17 @@
 import { postData } from './postData'
 
+const htmlValidatingExpression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+const htmlValidatingRegex = new RegExp(htmlValidatingExpression);
+
 function handleSubmit(event) {
     event.preventDefault()
 
     let formText = document.getElementById('textToAnalyze').value
     if (!formText){
         alert("Seems like you didn't put any text value into the input field. Please provide input for analysis.")
+        return;
+    } else if (!formText.match(htmlValidatingRegex)){
+        alert("Please provide a valid URI.")
         return;
     }
     postData(`http://localhost:8081/analyze`, {body : formText})
